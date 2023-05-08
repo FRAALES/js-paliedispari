303cceeba4befc4210e10f85a56cdfe1c9235870
FRAALES <alessandrofranco1992@gmail.com>
2023-05-08 23:10:35 +0200

    finito :)))

--- js/palindromo.js
+++ js/palindromo.js
@@ -8,24 +8,25 @@ let parolaUtente = prompt('Inserisci una parola:');
 let lunghezzaParolaUtente = parolaUtente.length;
 console.log(lunghezzaParolaUtente);
 
-//scompongo la parola a metà;
-let metaParola = lunghezzaParolaUtente / 2;
-console.log(metaParola);
-
-//Estraggo la prima metà della parola:
-let primaMetaParolaUtente = parolaUtente.substr(0, metaParola);
-console.log(primaMetaParolaUtente);
-
-
-//Estraggo la seconda metà della parola:
-let secondaMetaParolaUtente = parolaUtente.substr(-metaParola, metaParola);
-console.log(secondaMetaParolaUtente);
+//Se la prima parte della parola contiene le stesse identiche lettere della seconda parte della parola rigirate allora la parola è palindroma.
 
 
 //Creare una funzione per capire se la parola inserita è palindroma
 
+function stabilisciSePalindromo (parolaUtente){
+    //cicla attraverso metà parola
+    for (let i = 0; i < lunghezzaParolaUtente / 2; i++){
+        //controlla se la prima e la seconda metà sono uguali
+        if (parolaUtente[i] !== parolaUtente[lunghezzaParolaUtente - 1 - i]){
+            return 'non è un palindromo';
+        }else{
+            return 'è un palindromo';
+        }
+    }
+    
+}
 
 
-
-
-//Funzioni
+//Dichiara il valore della funzione
+let parolaPalindromaONo = stabilisciSePalindromo (parolaUtente);
+console.log(parolaPalindromaONo);
